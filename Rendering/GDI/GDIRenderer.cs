eb2c8485df2592e26f3c563ee0831275a5d9a99f
Никита Хуснутдинов <nikitahysnutdinov@gmail.com>
2025-08-24 07:02:55 +0300

    Доработка движка для отрисовки GDI

--- Rendering/GDI/GDIRenderer.cs
+++ Rendering/GDI/GDIRenderer.cs
@@ -1,20 +1,43 @@
-﻿namespace SharpNEX.Engine.Platform.Windows.Rendering.GDI
+﻿using System.Drawing;
+using System.Runtime.InteropServices;
+
+namespace SharpNEX.Engine.Platform.Windows.Rendering.GDI
 {
     internal class GDIRenderer : IWinRenderer
     {
+        private Graphics? _formGraphics;
+        private Graphics? _bufferGraphics;
+
+        private Bitmap? _buffer;
+
+        public void Init(IntPtr hwnd, int width, int height)
+        {
+            _buffer = new Bitmap(width, height);
+
+            _formGraphics = Graphics.FromHwnd(hwnd);
+            _bufferGraphics = Graphics.FromImage(_buffer);
+        }
+
         public void BeginFrame()
         {
-            throw new NotImplementedException();
+            Clear(255, 255, 255, 255);
         }
 
         public void EndFrame()
         {
-            throw new NotImplementedException();
+            var hwnd = _formGraphics!.GetHdc();
+            var rect = new Rectangle();
+            GetClientRect(hwnd, ref rect);
+            _formGraphics.ReleaseHdc(hwnd);
+
+            _formGraphics.DrawImage(_buffer!, rect);
         }
 
-        public void Clear(float r, float g, float b, float a)
-        {
-            throw new NotImplementedException();
+
+        public void Clear(int r, int g, int b, int a)
+        { 
+            var color = Color.FromArgb(a, r, g, b);
+            _bufferGraphics!.Clear(color);
         }
 
         public ITexture CreateTexture(string path)
@@ -29,7 +52,22 @@
 
         public void DrawTexture(ITexture texture, float x, float y, float width, float height)
         {
-            throw new NotImplementedException();
+            if (texture is not GDITexture gdiTexture)
+            {
+                throw new ArgumentException("Текстура должна быть типа GDITexture", nameof(texture));
+            }
+
+            var image = gdiTexture.GetImage();
+            var destRect = new RectangleF(x, y, width, height);
+
+            _bufferGraphics!.DrawImage(image, destRect);
         }
+
+        #region Import
+
+        [DllImport("user32.dll")]
+        public static extern bool GetClientRect(IntPtr hWnd, ref Rectangle rect);
+
+        #endregion
     }
 }
